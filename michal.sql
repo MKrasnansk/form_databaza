2e6d4120c4810cff0b4fa94488321269749431bc
MKrasnansk <miso.krasnansky@gmail.com>
2021-01-21 18:43:11 +0100

    databaza

---
+++ michal.sql
@@ -0,0 +1,23 @@
+-- --------------------------------------------------------
+-- Hostiteľ:                     localhost
+-- Verze serveru:                5.7.24 - MySQL Community Server (GPL)
+-- OS serveru:                   Win64
+-- HeidiSQL Verzia:              10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Exportování struktury databáze pro
+CREATE DATABASE IF NOT EXISTS `michal` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `michal`;
+
+-- Export dat nebyl vybrán.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
